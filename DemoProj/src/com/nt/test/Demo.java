f5e226a45d8b2a8f17447de6ef8effafcbd60444
oggy <oggy206@gmial.com>
2019-09-18 17:01:01 +0530

    initial commit and added new demo.java

---
+++ DemoProj/src/com/nt/test/Demo.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.nt.test;
+
+/**
+ * @author oggydewangan
+ *
+ */
+public class Demo {
+
+	/**
+	 * 
+	 */
+	public Demo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
