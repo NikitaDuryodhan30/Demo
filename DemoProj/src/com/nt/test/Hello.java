a90c6622708f2424696b09f2f8e5c698f39f7244
oggy <oggy206@gmial.com>
2019-09-18 17:06:32 +0530

    hello class added

---
+++ DemoProj/src/com/nt/test/Hello.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.nt.test;
+
+/**
+ * @author oggydewangan
+ *
+ */
+public class Hello {
+
+	/**
+	 * 
+	 */
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
